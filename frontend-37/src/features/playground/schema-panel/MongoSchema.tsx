6232a24e935ae74bda65f7a405603523a69d9670
err0r522 <47586934+err0r522@users.noreply.github.com>
2025-07-11 08:36:16 +0300

    refactor: fix inline styles

--- frontend-37/src/features/playground/schema-panel/MongoSchema.tsx
+++ frontend-37/src/features/playground/schema-panel/MongoSchema.tsx
@@ -1,31 +1,14 @@
 import { schemasStore } from "../schemasStore";
+import styles from "./MongoSchema.module.css";
 
 export function MongoSchema() {
   const { schemas } = schemasStore();
   if (!schemas) return;
 
   return (
-    <div
-      style={{
-        paddingTop: 15,
-        paddingRight: 20,
-        paddingBottom: 15,
-        paddingLeft: 5,
-        backgroundColor: "#E9E9FF",
-        borderRadius: 16,
-        width: "fit-content",
-        display: "flex",
-      }}
-    >
+    <div className={styles.wrapper}>
       {schemas.map((schema) => (
-        <div
-          style={{
-            fontSize: 24,
-            marginLeft: 15,
-            fontWeight: "bold",
-          }}
-          key={schema.name}
-        >
+        <div className={styles.item} key={schema.name}>
           {schema.name}
         </div>
       ))}
