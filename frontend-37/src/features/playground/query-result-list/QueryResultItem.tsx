af5f8c0c9bb1405a05cbd7106c7edc6546fbaa51
err0r522 <47586934+err0r522@users.noreply.github.com>
2025-07-11 06:30:23 +0300

    refactor(QueryResultItem): reduce unnecessary repetition

--- frontend-37/src/features/playground/query-result-list/QueryResultItem.tsx
+++ frontend-37/src/features/playground/query-result-list/QueryResultItem.tsx
@@ -9,57 +9,38 @@ interface QueryResultItemProps {
 }
 
 export function QueryResultItem({ result, queryNum }: QueryResultItemProps) {
-  if (!result.data) {
-    const rowsAffected = Math.max(0, result.rowcount);
-    return (
-      <div>
-        <details open>
-          <summary>
-            <span className={styles.queryTitle}>
-              {queryNum}. {result.query}
-            </span>
-            <span className={styles.execTime}>
-              <img
-                className={styles.clockImg}
-                src={clockImg}
-                alt="clock"
-                style={{ marginRight: 10 }}
-              />
-              {(result.execution_time * 1000).toFixed(3)}ms
-            </span>
-          </summary>
-          <p className={styles.affectedRows}>
-            <em>
-              {rowsAffected} {rowsAffected == 1 ? "row" : "rows"} affected.
-            </em>
-          </p>
-        </details>
-      </div>
-    );
-  } else {
-    let data = result.data;
-    return (
-      <div>
-        <details open>
-          <summary>
-            <span className={styles.queryTitle}>
-              {queryNum}. {result.query}
-            </span>
-            <span className={styles.execTime}>
-              <img
-                className={styles.clockImg}
-                src={clockImg}
-                alt="clock"
-                style={{ marginRight: 10 }}
-              />
-              {(result.execution_time * 1000).toFixed(3)}ms
-            </span>
-          </summary>
-          <div className={styles.resultTable}>
-            <SelectQueryResult queryData={data} />
-          </div>
-        </details>
-      </div>
-    );
-  }
+  const rowsAffected = Math.max(0, result.rowcount | 0);
+
+  const content = !result.data ? (
+    <p className={styles.affectedRows}>
+      <em>
+        {rowsAffected} {rowsAffected == 1 ? "row" : "rows"} affected.
+      </em>
+    </p>
+  ) : (
+    <div className={styles.resultTable}>
+      <SelectQueryResult queryData={result.data} />
+    </div>
+  );
+
+  return (
+    <div>
+      <details open>
+        <summary>
+          <span className={styles.queryTitle}>
+            {queryNum}. {result.query}
+          </span>
+          <span className={styles.execTime}>
+            <img
+              className={styles.clockImg}
+              src={clockImg}
+              alt="clock"
+            />
+            {(result.execution_time * 1000).toFixed(3)}ms
+          </span>
+        </summary>
+        { content }
+      </details>
+    </div>
+  );
 }
